8989096dadf6a52ade413733dadab1123511c298
David Morales Luis <dmoralesluis@danielcastelao.org>
2022-01-26 14:16:06 +0100

    david 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("Hola mundo");
         System.out.println("Commit B");
         System.out.println("david....");
+        System.out.println("morales....");
     }
 }
