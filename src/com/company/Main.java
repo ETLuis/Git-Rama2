0403bd54f7ab64212d0e1d24e10533230aff7d96
David Morales Luis <dmoralesluis@danielcastelao.org>
2022-01-26 14:16:22 +0100

    david 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("Commit B");
         System.out.println("david....");
         System.out.println("morales....");
+        System.out.println("luis !!");
     }
 }
